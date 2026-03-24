162c0813d7a2abee5f054d4bd12bb7129d96b470
Mehrad <mehrad77@gmail.com>
2026-03-25 00:28:40 +0330

    chore(main): release GsPlugin 2.3.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 // x-release-please-start-version
-[assembly: AssemblyVersion("2.3.2")]
-[assembly: AssemblyFileVersion("2.3.2")]
+[assembly: AssemblyVersion("2.3.3")]
+[assembly: AssemblyFileVersion("2.3.3")]
 // x-release-please-end
